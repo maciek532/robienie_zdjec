633064827650928ef2e1bef90f669c1e4dad8995
unknown <student@ST6.inf18.local>
2015-01-20 08:24:48 +0100

    add buttons

--- gen/com/example/robienie_zdjec/R.java
+++ gen/com/example/robienie_zdjec/R.java
@@ -13,12 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int LinearLayout1=0x7f060000;
+        public static final int button1=0x7f060001;
+        public static final int button2=0x7f060002;
+        public static final int button3=0x7f060003;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int edytuj_zdjecie=0x7f040003;
         public static final int hello_world=0x7f040001;
+        public static final int zapisz_zdjecie=0x7f040004;
+        public static final int zrob_zdjecie=0x7f040002;
     }
     public static final class style {
         /** 
